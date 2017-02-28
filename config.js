38a56fbe643bf73703216e1de46749b7bc0a4e9d
李继梁 <ljl@xxxx.com>
2017-02-28 10:28:07 +0800

    处理config.yml配置文件为json格式

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
  * @description 处理config.yml配置文件为json格式
  * @auth LJL
  */
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 const yaml = require('js-yaml');
 const _file = path.join(__dirname,'./config.yml');
 const config = yaml.safeLoad(fs.readFileSync(_file), 'utf-8');
