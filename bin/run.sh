934cd75598fbb8aa12bbeb4c045f3de21bf7fd97
liang <1951828835@qq.com>
2017-03-12 21:49:29 +0800

    更新启动脚本

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PATH=/usr/local/bin:/bin:/usr/bin
-export NODE_PATH=/usr/local/lib/node_modules
+#export PATH=/usr/local/bin:/bin:/usr/bin
+#export NODE_PATH=/usr/local/lib/node_modules
 
 pm2 flush
 pm2 start app.js -i max -n tServer
